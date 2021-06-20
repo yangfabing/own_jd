c0443ead0ccbec44b4935b8866eb2f1da21404d4
yangfabing <137946283@qq.com>
2021-06-20 14:47:12 +0800

    feat: update crontab,delete files

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -129,3 +129,5 @@
 15 0,12,22 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 #京喜牧场
 15 0,12,22 * * * node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
+#电竞经理
+15 10 * * * node /scripts/jd_djjl.js >> /scripts/logs/jd_djjl.log 2>&1
