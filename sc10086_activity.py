5c0e9aa37d929361c8c42b7d1793c038ef574d7f
yangfb <yangfb@hx168.com.cn>
2021-07-08 23:37:03 +0800

    Update sc10086_activity.py

--- sc10086_activity.py
+++ sc10086_activity.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*
 """
 四川移动APP-优享会员活动
+签到
 """
 
 import os
@@ -10,6 +11,8 @@ import time
 
 import requests
 import json
+import base64
+import random
 from messageInfo import message, get_message_info
 
 ss = requests.session()
@@ -39,17 +42,48 @@ else:
         pass
 ###################
 
+gifs = []
 
 def start():
     message(f"一共【{len(cookies)}】个账号")
+    # 先熊掌门相互助力
+    for ck in cookies:
+        sso_cookie = sso_cookies[cookies.index(ck)]
+        for share_phone in sso_cookies:
+            time.sleep(1)
+            help_draw(ck, sso_cookie, share_phone)
+
+    time.sleep(1)
     for ck in cookies:
         message(f"开始任务：第【{cookies.index(ck)+1}】个账号")
         sso_cookie = sso_cookies[cookies.index(ck)]
+        # 签到
         do_sign(ck, sso_cookie)
         time.sleep(1)
+        # 签到开宝箱
         sign_draw(sso_cookie)
         time.sleep(1)
+        # 熊掌门转转
         xzm_draw(sso_cookie)
+        time.sleep(1)
+        # 足球欧洲杯抽奖
+        epncup_draw(sso_cookie)
+        time.sleep(1)
+        share_phone, month = do_draw_gif(sso_cookie)
+
+        # 保存大礼包信息，最后再统一相互领取大礼包
+        gifs.append(
+            {
+                'share_phone': share_phone,
+                'month': month
+            }
+        )
+        print(gifs)
+
+    # 相互抽取优享大礼包，瓜分流量
+    for ck in cookies:
+        sso_cookie = sso_cookies[cookies.index(ck)]
+        draw_gif_ticket(ck, sso_cookie)
 
 
 def do_sign(ck, sso_cookie):
@@ -124,6 +158,94 @@ def xzm_draw(sso_cookie):
     }
     resp = ss.post(url=url, data=body).json()
     message(resp)
+
+
+def epncup_draw(sso_cookie):
+    print("足球欧洲杯抽奖")
+    url = 'https://wap.sc.10086.cn/scmccCampaign/epncup/doDraw.do'
+    body = {
+        'SSOCookie': sso_cookie,
+        'canals': 'zt1'
+    }
+    resp = ss.post(url=url, data=body).json()
+    print(resp)
+    if resp['result']['code'] == 0:
+        message("足球欧洲杯抽奖，中奖了")
+        message(f"{resp['result']['obj']['name']}")
+    else:
+        message(f"{resp['result']['info']}")
+
+
+def help_draw(ck, sso_cookie, share_phone):
+    print("熊掌门助力好友抽奖")
+    url = 'https://wap.sc.10086.cn/scmccCampaign/dzp2020help/helpDraw.do'
+    header = {
+            'XMLHttpRequest': 'XMLHttpRequest',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'zh-cn',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/4.4.2 scmcc.mobile',
+            'Connection': 'keep-alive',
+            'Cookie': ck
+    }
+    body = {
+        'SSOCookie': sso_cookie,
+        'canals': 'zt1',
+        'sharePhone': share_phone
+    }
+    resp = ss.post(url=url, headers=header, data=body).json()
+    print(resp)
+    if resp['result']['code'] == 0:
+        message(f"{resp['result']['info']}")
+        message(f"{resp['result']['obj']}")
+    else:
+        message(f"{resp['result']['info']}")
+
+
+def do_draw_gif(sso_cookie):
+    message("初始化生成优享礼包，分享出去共同瓜分流量")
+    url = 'https://wap.sc.10086.cn/scmccCampaign/oneVipGift/doDrawGift.do'
+    body = {
+        'SSOCookie': sso_cookie
+    }
+    resp = ss.post(url=url, data=body).json()
+    print(resp)
+    month = resp['result']['obj']['month']
+    share_phone = resp['result']['obj']['sharePhone']
+    jump_url = f"https://wap.sc.10086.cn/scmccCampaign/oneVipV2/index.html?channel=ztapp&SSOCookie={sso_cookie}"
+    bytes_url = jump_url.encode("utf-8")
+    share_url = f"http://wap.sc.10086.cn/scmccCampaign/oneVip/share.html?s={share_phone}&m={month}&jumpUrl={base64.b64encode(bytes_url).decode()}"
+    message(f"生成大礼包url成功")
+    message(share_url)
+    return share_phone, month
+
+
+def draw_gif_ticket(ck, sso_cookie):
+    for dic in gifs:
+        time.sleep(1)
+        share_phone = dic['share_phone']
+        month = dic['month']
+        message(f"【{sso_cookie}】抽取大礼包,sharePhone:{share_phone}, month:{month}")
+        key = ''.join(str(random.choice(range(1, 10))) for _ in range(16))
+        url = f"http://wap.sc.10086.cn/scmccCampaign/oneVipGift/drawGiftTicket.do?key=0.{key}"
+        header = {
+            'XMLHttpRequest': 'XMLHttpRequest',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Language': 'zh-cn',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/4.4.2 scmcc.mobile',
+            'Connection': 'keep-alive',
+            'Cookie': ck
+        }
+        body = {
+            'SSOCookie': sso_cookie,
+            's': share_phone,
+            'm': month
+        }
+        resp = ss.post(url=url, headers=header, data=body).json()
+        print(resp)
 
 
 if __name__ == '__main__':
