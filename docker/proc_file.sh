625fc5f69ca63895abf8118922b3c56759bb5198
yangfb <yangfb@hx168.com.cn>
2021-07-13 21:01:16 +0800

    update

--- docker/proc_file.sh
+++ docker/proc_file.sh
@@ -26,10 +26,4 @@ else
    fi
 fi
 
-echo "处理财富岛jd_cfd_loop任务。。。"
-echo "默认启用jd_cfd_loop杀掉jd_cfd_loop任务，并重启"
-eval $(ps -ef | grep "jd_cfd_loop" | grep -v "grep" | awk '{print "kill "$1}')
-echo '' >/scripts/logs/jd_cfd_loop.log
-eval $(ts-node /scripts/jd_cfd_loop.ts | ts >>/scripts/logs/jd_cfd_loop.log 2>&1 &)
-echo "默认jd_cfd_loop重启完成"
 
