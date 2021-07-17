41e6faab09779d994bb2f9c1684cfe3317a3bd6d
yangfb <yangfb@hx168.com.cn>
2021-07-17 18:43:44 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -95,8 +95,6 @@
 21 1,6 * * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1
 #京喜财富岛
 5 0-23/1 * * * ts-node /scripts/jd_cfd.ts | ts >> /scripts/logs/jd_cfd.log 2>&1
-#京喜财富岛提现
-# 0 0 * * * node /scripts/jx_cfdtx.js >> /scripts/logs/jx_cfdtx.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
 #京东直播（又回来了）
@@ -193,8 +191,5 @@
 20 * * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
 #京喜签到
 5 0 * * * node /scripts/jd_jxqd.js >> /scripts/logs/jd_jxqd.log 2>&1
-#京东宠汪汪
-15 0-23/2 * * * node /scripts/jd_joy.js >> /scripts/logs/jd_joy.log 2>&1
-#京东宠汪汪偷积分狗粮
-10 0-21/3 * * * node /scripts/jd_joy_steal.js >> /scripts/logs/jd_joy_steal.log 2>&1
+
 
