fe8755d5b64a392c62132eec0ca3e03fa0fa2ffe
yangfb <yangfb@hx168.com.cn>
2021-07-18 17:05:44 +0800

    update

---
+++ tools/jxtoken.ts
@@ -0,0 +1,24 @@
+/**
+ * 可以从cfd提现任意金额抓包
+ * url: https://m.jingxi.com/jxbfd/user/CashOut
+ * 从url中找参数的值
+ */
+let Jxtoken = [
+  {
+    strPgUUNum: "23d0d61c4da326a2e92d9a0639cb31a6",
+    strPgtimestamp: "1626598571546",
+    strPhoneID: "1f16c12de39bc3f9b97715f2790d621eb92c950c"
+  },
+  {
+    strPgUUNum: "",
+    strPgtimestamp: "",
+    strPhoneID: ""
+  },
+  {
+    strPgUUNum: "",
+    strPgtimestamp: "",
+    strPhoneID: ""
+  },
+]
+
+export default Jxtoken
